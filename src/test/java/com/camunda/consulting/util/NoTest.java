762db40811d352b846a611b89461d81c12ea69a3
Bernd Rücker <bernd.ruecker@camunda.com>
2017-07-12 09:29:52 +0200

    added empty test for jenkins

---
+++ src/test/java/com/camunda/consulting/util/NoTest.java
@@ -0,0 +1,11 @@
+package com.camunda.consulting.util;
+
+import org.junit.Test;
+
+public class NoTest {
+
+  @Test
+  public void nothinToTest() {
+    // empty test to make Jenkins happy - but no junit test case necessary in here
+  }
+}
